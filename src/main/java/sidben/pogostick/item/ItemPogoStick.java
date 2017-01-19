6f5f33f1cb39440bacd3ac1c4626bee51a2d9788
sidben <sidbenetti@gmail.com>
2017-01-19 05:04:53 -0200

    Custom model for the pogo stick when holding and using (1st and 3rd person)

--- src/main/java/sidben/pogostick/item/ItemPogoStick.java
+++ src/main/java/sidben/pogostick/item/ItemPogoStick.java
@@ -1,15 +1,21 @@
 package sidben.pogostick.item;
 
+import javax.annotation.Nullable;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.EnumAction;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import sidben.pogostick.helper.LogHelper;
 import sidben.pogostick.reference.Reference;
 
@@ -23,6 +29,17 @@ public class ItemPogoStick extends Item
         this.setRegistryName("pogo_stick");
         this.setCreativeTab(CreativeTabs.TRANSPORTATION);
         this.setMaxDamage(720);
+
+        this.addPropertyOverride(new ResourceLocation("using"), new IItemPropertyGetter()
+        {
+            @SideOnly(Side.CLIENT)
+            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
+            {
+                // TODO: use the isEntityUsingPogoStick(), that should be moved elsewhere
+                return entityIn != null && entityIn.isHandActive() && entityIn.getActiveItemStack() == stack ? 1.0F : 0.0F;
+            }
+        });
+
     }
 
 
